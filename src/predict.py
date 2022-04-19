9b1c7c8ed0e41b3cb759e22466f7c39f4b6d3731
LeGourmet <92042633+LeGourmet@users.noreply.github.com>
2022-04-19 21:35:30 +0200

    video

--- src/predict.py
+++ src/predict.py
@@ -11,34 +11,45 @@ from absl import app
 def predictVideo(model):
     video_in = cv2.VideoCapture("../video/video.mkv")
     # data of input video
-    fps = int(video_in.get(cv2.CAP_PROP_FPS))
+    nbFrame = int(video_in.get(cv2.CAP_PROP_FRAME_COUNT)-1)
+    nbFrame = 50
     width = int(video_in.get(cv2.CAP_PROP_FRAME_WIDTH))
+    width = 160#400#800
     height = int(video_in.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    height = 90#225#450
 
     # create output video
     fourcc = cv2.VideoWriter_fourcc(*'HFYU') # ou *'XVID' ,*MJPG, *MP4V, cv2.CV_FOURCC_PROMPT
-    video_out = cv2.VideoWriter('../video/output_video.avi', fourcc, fps*2, (width, height))
+    video_out = cv2.VideoWriter('../video/output_video.avi', fourcc, int(video_in.get(cv2.CAP_PROP_FPS))*2, (width, height))
 
     ret, frame1 = video_in.read()
     if ret:
         img1 = cv2.resize(cv2.cvtColor(frame1, cv2.COLOR_BGR2RGB), (width, height))
-        imgPred = np.zeros_like(img1)
-        print(img1.shape)
+        img2 = None
+        imgPred = None
 
-        for _ in range(fps-1):
+        for i in range(nbFrame-1):
             _, frame2 = video_in.read()
             img2 = cv2.resize(cv2.cvtColor(frame2, cv2.COLOR_BGR2RGB), (width, height))
             X1X2 = np.concatenate((img1, img2), axis=2)
-            X1X2 = np.reshape(X1X2,(1,X1X2.shape[0],X1X2.shape[1],X1X2.shape[2]))
-            bar = tqdm(total=width*height)
-            for x in range(39,height+38):
-                for y in range(39,width+38):
-                    pix = model.predict(X1X2[:,x-39:x+40, y-39:y+40, :])
+            X1X2 = np.pad(X1X2,((39,39),(39,39),(0,0)))
+            imgPred = []
+
+            bar = tqdm(total=width*height, desc="Image "+str(i+1)+"/"+str(nbFrame-1))
+            for x in range(39, height + 39):  # batch need to be equal all image
+                batch = []
+                for y in range(39, width + 39):
+                    batch.append(X1X2[x - 39:x + 40, y - 39:y + 40, :])
                     bar.update()
+                batch = np.array(batch)
+                imgPred.append(model.predict(batch))
+
             video_out.write(img1)
-            video_out.write(imgPred)
+            tmp = np.array(imgPred)
+            video_out.write(tmp)
             img1 = img2
 
+        video_out.write(img2) # marche pas
     video_out.release()
     video_in.release()
 
@@ -59,7 +70,7 @@ def predictImage(model):
         X1X2 = np.pad(X1X2,((39,39),(39,39),(0,0)))
 
         bar = tqdm(total=width*height)
-        for x in range(39, height+39):
+        for x in range(39, height+39):  # batch need to be equal all image
             batch = []
             for y in range(39, width+39):
                 batch.append(X1X2[x - 39:x + 40, y - 39:y + 40, :])
@@ -85,9 +96,9 @@ def predictImage(model):
 
 
 def load_model(model):
-    if os.path.isfile("./trained_model/model.h5"):
+    if os.path.isfile("../trained_model/model.h5"):
         print("Loading model from model.h5")
-        model.load_weights("./trained_model/model.h5")
+        model.load_weights("../trained_model/model.h5")
     else:
         print("model.h5 not found")
     model.summary()
@@ -96,7 +107,10 @@ def load_model(model):
 def main(argv):
     model = AutoEncoder()
     load_model(model)
-    predict(model)
+    predictVideo(model)
+    video_in = cv2.VideoCapture("../video/output_video.avi")
+    nbFrame = int(video_in.get(cv2.CAP_PROP_FRAME_COUNT) - 1)
+    print(nbFrame)
 
 
 if __name__ == '__main__':
--- src/train.py
+++ src/train.py
@@ -15,15 +15,15 @@ from data_manager import DataManager
 from tensorflow.keras.optimizers import Adamax
 from tensorflow.keras.losses import MSE
 
-flags.DEFINE_integer("epochs", 2, "number of epochs")
-flags.DEFINE_integer("batch_size", 2, "batch size")
-flags.DEFINE_integer("nbPixelsPick", 2, "number of pixel pick by batch") # use 128 in paper
-flags.DEFINE_float("learning_rate", 0.001, "learning rate")
+flags.DEFINE_integer("epochs", 10, "number of epochs")
+flags.DEFINE_integer("batch_size", 32, "batch size")
+flags.DEFINE_integer("nbPixelsPick", 128, "number of pixel pick by batch")  # use 128 in paper
+flags.DEFINE_float("learning_rate", 0.001, "learning rate")                 # use 0.001 in paper
 FLAGS = flags.FLAGS
 
 
 def train(model):
-    manager = DataManager(flag=True)
+    manager = DataManager()
     nbBatches = manager.nbBatches // FLAGS.batch_size
     lossTab = []
     loss = 0
@@ -37,9 +37,8 @@ def train(model):
             X1X2 = np.concatenate((X1,X2), axis=3)
 
             for _ in range(FLAGS.nbPixelsPick):
-                # x and y are invert in datamanager
-                x = randint(39, manager.height+38)
-                y = randint(39, manager.width+38)
+                x = randint(39, manager.height+38)  # why 38 investigate
+                y = randint(39, manager.width+38)   # why 38 investigate
                 loss = model.train_on_batch(X1X2[:, x-39:x+40, y-39:y+40, :], np.squeeze(Y[:, x:x+1, y:y+1, :]))
 
         lossTab.append(loss)
@@ -47,7 +46,7 @@ def train(model):
 
     print("Finished training.")
 
-    # model.save("./trained_model/model.h5")
+    model.save("../trained_model/model.h5")
     predictImage(model)
 
     plt.plot(lossTab)
@@ -61,9 +60,9 @@ def custom_loss(y_true,y_pred):
 
 
 def load_model(model):
-    if os.path.isfile("./trained_model/model.h5"):
+    if os.path.isfile("../trained_model/model.h5"):
         print("Loading model from model.h5")
-        model.load_weights("./trained_model/model.h5")
+        model.load_weights("../trained_model/model.h5")
     else:
         print("model.h5 not found")
 
